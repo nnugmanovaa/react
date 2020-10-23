4720c3dc3237f997295eddcb6888d3ab04f76ac9
Nargiza <nnugmanovaa@gmail.com>
2020-10-23 11:04:29 +0600

    details

--- src/components/recipeDetails/RecipeDetails.tsx
+++ src/components/recipeDetails/RecipeDetails.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState, useRef } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import { recipeList, RecipeModel } from '../../models/Recipe';
 import './details.css'
@@ -8,9 +8,18 @@ interface Props {
 }
 
 export default function RecipeDetails({}: Props): ReactElement {
+    const [email, setEmail] = useState("")
+    const inputRef = React.useRef<HTMLInputElement>(null)
     const match = useRouteMatch<{ id: string }>();
     const item = (recipeList.find(recipe => recipe.id.toString() === match.params.id) || recipeList[0]) ;
 
+    const handleSubmit = async (
+        e: React.FormEvent<HTMLFormElement>
+      ): Promise<void> => {
+        e.preventDefault();
+    
+        console.log(inputRef.current?.value)
+      };
     return (
             <div>
                 <div className="itemHeader">
@@ -43,6 +52,36 @@ export default function RecipeDetails({}: Props): ReactElement {
                             {item.notes}
                        </div>
                     </div>
+                 </div>
+                 <div className="email">
+                    <div className="emailInner">
+                       <div className="emailTextFirst">
+                        <strong><h2>Want more Food52?</h2></strong>
+                        </div> 
+                        <div className="emailTextlast">
+                        <h4>Our best tips for eating thoughtfully and <br/> 
+                        living joyfully, right to your inbox.</h4>
+                        </div>
+                        <div>
+                            <form onSubmit={handleSubmit}>
+                                <input ref={inputRef} className="mailInput" type="text" placeholder="hello@food52.com"/>
+                                <button className="btn-email" type="submit">I'm in</button>
+                            </form>
+                          
+                        </div>
+                    </div>
+                 </div>
+                 <div id="comment">
+                     <div className="commentReview">
+                     &#x1f4ac; {item.review_count} REVIEWS
+                     </div>
+                     <div>
+                     <textarea className="commetArea" placeholder="Leave a Review">
+                    </textarea>
+                     </div>
+                     <div>
+                         <button className="btnReview">SUBMIT REVIEW</button>
+                     </div>
                  </div>
             </div>
             
